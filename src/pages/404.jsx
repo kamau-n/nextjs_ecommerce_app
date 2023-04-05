f65a3ea863d4871f481a270feeacb6c1f5a5f607
kamau-n <bsclmr146620@spu.ac.ke>
2023-04-05 23:55:31 +0300

    another commit

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function pageNotFound() {
+  return (
+    <div className="w-1/2 m-auto bg-slate-400 p-16">
+      <h2>It seems the page you are looking for is not found</h2>
+      <Link href="/">Go back to homepage</Link>
+    </div>
+  );
+}
--- src/pages/products/shoes/index.jsx
+++ src/pages/products/shoes/index.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 
+import data from "../../../resources/shoes-products";
+import nav_bar from "@/components/nav_bar";
+
 export default function shoes() {
-  return <div>shoes page</div>;
+  const dispaly = () => {
+    // console.log(data);
+    return data.map((product) => {
+      return (
+        <div className="w-3/4 p-10 mx-auto my-10  bg-slate-300">
+          <span>{product.id}</span>
+          <span>{product.name}</span>
+          <img
+            src={product.imageURL}
+            alt="no image"
+            width="220"
+            height="220"
+            placeholder="blur"
+          />
+        </div>
+      );
+    });
+  };
+  return (
+    <div>
+      <h2 className="uppercase font-bold text-center p-4 m-4 font-mono after:text-blue-500">
+        Available Shoes
+      </h2>
+      <div className="p-5 m-5  grid grid-cols-4 gap-4">{dispaly()}</div>
+    </div>
+  );
 }
